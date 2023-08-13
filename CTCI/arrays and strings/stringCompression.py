3cfbccb1bc44011fb4c191fec2e24409978cba3c
Shivam Chawla <shivamchawla9@gmail.com>
2023-08-13 09:17:54 +0530

    arrays and string

---
+++ CTCI/arrays and strings/stringCompression.py
@@ -0,0 +1,6 @@
+"""
+String Compression : Implement a method to perform basic string compression using
+                     the counts of repeated characters. For example, the string
+                     aabcccccaaa would become a2b1c5a3. If the 'compressed' string 
+                     would not 
+"""
